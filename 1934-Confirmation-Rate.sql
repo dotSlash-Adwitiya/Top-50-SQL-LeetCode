c6432f8dc9553d760e1712d06f218b59d821d85e
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-21 14:22:32 +0530

    Added CTE solution

--- 1934-Confirmation-Rate.sql
+++ 1934-Confirmation-Rate.sql
@@ -41,3 +41,32 @@ AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c USING (user_id)
 GROUP BY s1.user_id;
+
+-- # Method - 3 Using AVG()
+SELECT s.user_id,ROUND(
+     AVG(if(c.action = 'confirmed', 1, 0)), 2) as confirmation_rate
+FROM Signups s 
+LEFT JOIN Confirmations c USING(user_id) GROUP BY c.user_id; 
+
+
+-- # Method - 4 : Using Common Table Expressions (CTE)
+
+WITH cte AS (
+    SELECT a.user_id,
+           CASE WHEN b.action = 'confirmed' THEN 1 ELSE 0 END AS confirmed_user,
+           CASE WHEN b.action = 'timeout' THEN 1 ELSE 0 END AS timeout_user
+    FROM Signups a
+    LEFT JOIN Confirmations b ON a.user_id = b.user_id
+),
+cte2 AS (
+    SELECT user_id, 
+           SUM(confirmed_user) AS total_confirmed,
+           SUM(timeout_user) AS total_timeout,
+           SUM(confirmed_user + timeout_user) AS total_request
+    FROM cte
+    GROUP BY user_id
+)
+SELECT user_id, 
+       ROUND(SUM(IFNULL(total_confirmed / total_request, 0)), 2) AS CONFIRMATION_RATE
+FROM cte2
+GROUP BY user_id;

ccfb7205a374642ed3fe7327babe25e38ded1f48
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-21 14:30:57 +0530

    Q15 - Boring - Movies

---
+++ 620-Boring-Movies.sql
@@ -0,0 +1,6 @@
+
+# Problem Link : https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+-- Problem statement : Fetch the movies which has their description NOT Boring and id is odd. 
+
+SELECT * FROM Cinema WHERE id % 2 = 1 AND description <> "boring"
+ORDER BY rating DESC;

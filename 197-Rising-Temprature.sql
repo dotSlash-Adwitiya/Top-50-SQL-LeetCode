50c5860eba445cd7fde8b41d8364418d4235cb04
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-23 20:23:22 +0530

    Solved without using DATEDIFF() or INTERVALe, 14/15 Test Cases Passed

--- 197-Rising-Temprature.sql
+++ 197-Rising-Temprature.sql
@@ -6,5 +6,12 @@
 
 -- # Method - Use SELF-JOIN, DATEDIFF() is the function which subtracts 2 dates, 1st param > 2nd param to get a +ve value
 SELECT w2.id FROM  Weather w1 
+-- Order of parameters passed to DATEDIFF() matters, the 1st Parameter must be > 2nd parameter
 JOIN Weather w2 ON DATEDIFF(w2.recordDate, w1.recordDate) = 1 
 AND w1.temperature < w2.temperature;
+
+
+-- Following code will pass 14/15 test cases.
+SELECT w2.id FROM Weather w1
+INNER JOIN Weather w2 ON (DAY(w2.recordDate)-1 = DAY(w1.recordDate))
+WHERE w2.temperature > w1.temperature

1db800b89db71eadd8994705027e6a7f53e4a889
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-01-27 22:18:18 +0530

    Practice-1 commit

--- 1378-replace-employeID-with-Unique-identifier.sql
+++ 1378-replace-employeID-with-Unique-identifier.sql
@@ -11,3 +11,9 @@ LEFT JOIN EmployeeUNI e2 ON e1.id = e2.id;
 SELECT e2.unique_id, e1.name
 FROM Employees e1
 LEFT JOIN EmployeeUNI e2 USING(id);
+
+# PRACTICE
+SELECT e2.unique_id, e1.name 
+    FROM Employees e1 
+LEFT JOIN EmployeeUNI e2 ON
+(e1.id = e2.id);

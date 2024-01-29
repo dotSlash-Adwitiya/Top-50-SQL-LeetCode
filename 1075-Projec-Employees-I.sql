4d176bcde20f29a5a247db8287ec6b9c21ad4b54
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-01-29 22:03:55 +0530

    29-Jan-2024 - Learnt, which column to GROUP BY

--- 1075-Projec-Employees-I.sql
+++ 1075-Projec-Employees-I.sql
@@ -7,3 +7,17 @@ GROUP BY p.project_id;
 
 -- GROUPING is not neccesarily done by the column we're trying to apply the aggregate() on !
 -- It can be even done by the columns which we're trying to fetch
+
+-- Practice - 1 : 29-Jan-2024
+-- Learning : From which column, we need to perform a GROUP BY. 
+-- Keywords to identify which column to perform a group by -> "For each - Project", Hence - project_id.
+SELECT p.project_id, 
+ROUND(
+    AVG(
+        e.experience_years
+        )
+    ,2) AS average_years
+FROM Project p 
+INNER JOIN Employee e 
+USING (employee_id)
+GROUP BY project_id;
